0b71a957d551f57b41e53ff50e456fb161327711
Harley Rayner <harleymteden@gmail.com>
2018-09-28 21:04:44 +0000

    defined some CLI behaviors in CLI.rb with fake data

--- lib/soundcloud_scraper/cli.rb
+++ lib/soundcloud_scraper/cli.rb
@@ -4,6 +4,15 @@ class SoundcloudScraper::CLI
 
   def call
     puts "Today's hottest tracks"
+    list_tracks
+    menu
   end
-
-end
+  
+  def list_tracks 
+    puts "1. lil peep - dude"
+    puts "lil Ruby - xan"
+  end
+  
+  def menu 
+    puts "enter the number of the track you'd like more info on"
+  end 
