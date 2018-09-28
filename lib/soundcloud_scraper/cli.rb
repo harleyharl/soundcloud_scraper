9a34fb47f9646f886b5c4e8de8bf391288be00fa
Harley Rayner <harleymteden@gmail.com>
2018-09-28 21:16:31 +0000

    built out more of CLI with fake data for now

--- lib/soundcloud_scraper/cli.rb
+++ lib/soundcloud_scraper/cli.rb
@@ -14,5 +14,17 @@ class SoundcloudScraper::CLI
   end
   
   def menu 
-    puts "enter the number of the track you'd like more info on"
+    puts "enter the number of the track you'd like more info on or type exit"
+    while input != "exit"
+     input = gets.strip.downcase
+     case input 
+      when "1"
+       puts "more info on track 1.."
+      when "2"
+       puts "more info on track 2"
+    end 
+  end 
+  
+  end 
+        
   end 
