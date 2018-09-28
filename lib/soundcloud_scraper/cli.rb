84aaa24be25e1d25deaf762db87c7fd20c18ad7d
Harley Rayner <harleymteden@gmail.com>
2018-09-28 21:22:35 +0000

    stubbed out more of CLI

--- lib/soundcloud_scraper/cli.rb
+++ lib/soundcloud_scraper/cli.rb
@@ -6,20 +6,22 @@ class SoundcloudScraper::CLI
     puts "Today's hottest tracks"
     list_tracks
     menu
+    goodbye
   end
   
   def list_tracks 
     puts "1. lil peep - dude"
-    puts "lil Ruby - xan"
+    puts "2. lil Ruby - xan"
   end
   
   def menu 
-    puts "enter the number of the track you'd like more info on or type exit"
+    input = nil 
     while input != "exit"
+    puts "enter the number of the track you'd like more info on or type exit"
      input = gets.strip.downcase
      case input 
       when "1"
-       puts "more info on track 1.."
+       puts "more info on track 1"
       when "2"
        puts "more info on track 2"
     end 
@@ -27,4 +29,8 @@ class SoundcloudScraper::CLI
   
   end 
         
+    def goodbye 
+      puts "see you tomorrow for more hot tracks"
+    end 
+    
   end 
